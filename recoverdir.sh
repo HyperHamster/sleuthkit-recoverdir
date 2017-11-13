a09ba7fa678d1f9713f7a8642e5cc32d103bf838
HyperHamster <HyperHamster@users.noreply.github.com>
2017-11-13 13:31:09 -0600

    Update help message

--- recoverdir.sh
+++ recoverdir.sh
@@ -33,13 +33,13 @@ stopwatch -s
 # Print Help Message
 help_message="\
 $(b USAGE)
-    recoverdir.sh IMAGE_FILE BASE_INODE [EXCEPT_INODE ...]
+    recoverdir.sh IMAGE BASE_INODE [EXCEPT_INODE]...
 $(b OPTIONS)
-    -h  Print this help message, then exit successfully.
-    -R  Do not recover files, instead exit successfully.
-    -s  Calculate the size of all catalogued files and directories.
-    -q  Do not print catalogue entries, time elapsed, or recovery operations.
-    -b  Do not recover files or directories deeper than base level."
+    -h      Print this help message, then exit successfully.
+    -R      Do not recover files, instead exit successfully.
+    -s      Calculate the size of all catalogued files and directories.
+    -q      Do not print catalogue entries, time elapsed, or recovery operations.
+    -b      Do not recover files or directories deeper than base level."
 print_help() {
     echo -e "$help_message"
     exit 0
